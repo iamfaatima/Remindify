6feed875acadb9dac46b595e793d45c1bf6c1e03
Dev <iamfaatima@gmail.com>
2023-11-10 20:55:49 +0500

    implemented alarm functionality with user notifications

--- Remindify/Remindify/Controllers/AddReminderViewController.swift
+++ Remindify/Remindify/Controllers/AddReminderViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 import FirebaseFirestore
 import DateTimePicker
 import FirebaseAuth
+import UserNotifications
+import AVFoundation
 
-class AddReminderViewController: UIViewController, DateTimePickerDelegate {
+class AddReminderViewController: UIViewController, DateTimePickerDelegate, UNUserNotificationCenterDelegate {
     
     let db = Firestore.firestore()
     
+    var alarmDate: Date?
+    var audioPlayer: AVAudioPlayer?
+    var isAlarmRinging = false
+       
+    let dateFormatter = DateFormatter()
     @IBOutlet weak var warningLabel: UILabel!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
@@ -25,6 +32,10 @@ class AddReminderViewController: UIViewController, DateTimePickerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         warningLabel.isHidden = true
+        // Request notification authorization
+        requestNotificationAuthorization()
+        UNUserNotificationCenter.current().delegate = self
+
         
     }
     
@@ -35,8 +46,8 @@ class AddReminderViewController: UIViewController, DateTimePickerDelegate {
         picker.frame = CGRect(x: 0, y: 100, width: picker.frame.size.width, height: picker.frame.size.height)
         
         picker.completionHandler = { date in
-            let formatter = DateFormatter()
-            formatter.dateFormat = "hh:mm:ss aa dd/MM/YYYY"
+            
+            self.dateFormatter.dateFormat = "HH:mm dd/MM/yyyy"
             //self.title = formatter.string(from: date)
         }
         picker.delegate = self
@@ -77,10 +88,93 @@ class AddReminderViewController: UIViewController, DateTimePickerDelegate {
                     }
                 }
             }
+            print(selectedDate)
+            if let date = selectedDate{
+                scheduleAlarmNotification(at: date)
+            }
+            
         }
         
         //navigate to home
         let homeViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeReminderTableViewController") as! HomeReminderTableViewController
         self.navigationController?.pushViewController(homeViewController, animated: true)
     }
+    
+    // MARK: - Notification Functions
+    func requestNotificationAuthorization() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if granted {
+                print("Notification authorization granted")
+            } else {
+                print("Notification authorization denied or error")
+            }
+        }
+    }
+    
+    // Implement the notification delegate method to handle notifications when the app is in the foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // Handle the notification presentation when the app is in the foreground (e.g., show an alert)
+        if notification.request.identifier == "alarmNotification" {
+            play()
+            isAlarmRinging = true
+        }
+        completionHandler([.alert, .sound, .badge])
+    }
+    
+    func play() {
+        print("Playing alarm sound")
+        if let soundURL = Bundle.main.url(forResource: "A", withExtension: "wav", subdirectory: "Sounds") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+                audioPlayer!.play()
+            } catch {
+                print("Error playing alarm sound: \(error.localizedDescription)")
+            }
+        } else {
+            print("Error: Sound file not found")
+        }
+    }
+    
+    func scheduleAlarmNotification(at date: String) {
+        let content = UNMutableNotificationContent()
+        content.title = "Alarm"
+        content.body = "Time to wake up!"
+        content.sound = UNNotificationSound.default
+
+        // Play the "A.wav" sound when the notification is scheduled
+        if let soundURL = Bundle.main.url(forResource: "A", withExtension: "wav", subdirectory: "Sounds") {
+            let alarmSound = UNNotificationSound(named: .init(rawValue: soundURL.relativeString))
+            content.sound = alarmSound
+        }
+
+        let calendar = Calendar.current
+        dateFormatter.dateFormat = "HH:mm dd/MM/yyyy" // Corrected date format
+
+        if let sdate = dateFormatter.date(from: date) {
+            print(sdate)
+            alarmDate = sdate
+
+            let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute], from: alarmDate!)
+
+            print(dateComponents)
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+
+            let identifier = "alarmNotification"
+
+            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+
+            let center = UNUserNotificationCenter.current()
+            center.add(request) { (error) in
+                if let error = error {
+                    print("Error scheduling notification: \(error.localizedDescription)")
+                } else {
+                    print("Alarm notification scheduled successfully")
+                }
+            }
+        } else {
+            print("Date parsing failed.")
+        }
+    }
+
 }
